3718c495cc7c70e7bc8a35717bfd177c3d181dda
Prateek Agarwal <agarwal@yelp.com>
2016-01-11 15:51:21 -0800

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     provides=["pyramid_zipkin"],
     author='Yelp, Inc.',
     author_email='opensource+pyramid-zipkin@yelp.com',
-    license='Copyright Yelp 2015',
+    license='Copyright Yelp 2016',
     url="https://github.com/Yelp/pyramid_zipkin",
     description='Zipkin distributed tracing system support library for pyramid.',
     packages=find_packages(exclude=('tests*', 'testing*', 'tools*')),
@@ -20,5 +20,16 @@ setup(
     install_requires = [
         'pyramid',
         'thriftpy',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Apache v2 License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
 )
