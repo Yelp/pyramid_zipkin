3fdc6b15162701268742e0564832a72dbf98f2d9
Jeffrey Ying <mying@yelp.com>
2017-08-16 11:10:16 -0700

    Always generate trace id

--- tests/tween_test.py
+++ tests/tween_test.py
@@ -1,34 +1,23 @@
 import mock
 
 from pyramid_zipkin import tween
+import pytest
 
 
+@pytest.mark.parametrize('is_tracing', [True, False])
 @mock.patch('pyramid_zipkin.tween.zipkin_span', autospec=True)
-def test_zipkin_tween_not_sampled(mock_span, dummy_request, dummy_response):
+def test_zipkin_tween_sampling(
+    mock_span,
+    dummy_request,
+    dummy_response,
+    is_tracing
+):
     """
-    If the request is not sampled, we shouldn't use the
-    py_zipkin context manager
+    We should enter py_zipkin context manager and
+    generate a trace id regardless of whether we are sampling
     """
     dummy_request.registry.settings = {
-        'zipkin.is_tracing': lambda _: False,
-        'zipkin.transport_handler': lambda _: None,
-    }
-    handler = mock.Mock()
-    handler.return_value = dummy_response
-
-    assert tween.zipkin_tween(handler, None)(dummy_request) == dummy_response
-    assert handler.call_count == 1
-    assert mock_span.call_count == 0
-
-
-@mock.patch('pyramid_zipkin.tween.zipkin_span', autospec=True)
-def test_zipkin_tween_sampled(mock_span, dummy_request, dummy_response):
-    """
-    If the request is sampled, we should wrap the handler in the
-    py_zipkin context manager
-    """
-    dummy_request.registry.settings = {
-        'zipkin.is_tracing': lambda _: True,
+        'zipkin.is_tracing': lambda _: is_tracing,
         'zipkin.transport_handler': lambda _: None,
     }
     handler = mock.Mock()
