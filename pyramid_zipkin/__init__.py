eab6521149261efc27132243d1198b9e58ce2253
Daniele Rolando <drolando@yelp.com>
2020-04-06 14:52:36 -0700

    Change tween import order to be close to INGRESS
    We want the zipkin tween to be close to INGRESS so that it'll also wrap
    all other tweens. under INGRESS implicitly also implies over=EXCVIEW
    since INGRESS is before EXCVIEW.

--- pyramid_zipkin/__init__.py
+++ pyramid_zipkin/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from pyramid.tweens import EXCVIEW
+from pyramid.tweens import INGRESS
 
 
 def includeme(config):  # pragma: no cover
     """
     :type config: :class:`pyramid.config.Configurator`
     """
-    config.add_tween('pyramid_zipkin.tween.zipkin_tween', over=EXCVIEW)
+    config.add_tween('pyramid_zipkin.tween.zipkin_tween', under=INGRESS)
--- tests/acceptance/server_span_test.py
+++ tests/acceptance/server_span_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import time
 import json
+import time
 
 import mock
 import pytest
