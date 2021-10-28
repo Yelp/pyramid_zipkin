15f06fe3ba39a674bb64a7ede6d3eff2d5c43d9b
Amit Mokal <amokal@yelp.com>
2021-10-28 15:01:55 -0700

    fix flake8 issue

--- pyramid_zipkin/tween.py
+++ pyramid_zipkin/tween.py
@@ -196,7 +196,11 @@ def zipkin_tween(handler, registry):
             )
 
             if zipkin_settings.post_handler_hook:
-                zipkin_settings.post_handler_hook(request, response, zipkin_context)
+                zipkin_settings.post_handler_hook(
+                    request,
+                    response,
+                    zipkin_context
+                )
 
             return response
 
